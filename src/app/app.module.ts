8fca360ffae24763e8824ede5a2b55402bfee182
¨Franco <elementof77752@gmail.com>
2018-11-20 10:13:07 -0300

    Gitignore vacio

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,17 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { BodyComponent } from './body/body.component';
 import { FooterComponent } from './footer/footer.component';
+import { MapsComponent } from './maps/maps.component';
+import { EventComponent } from './event/event.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     BodyComponent,
-    FooterComponent
+    FooterComponent,
+    MapsComponent,
+    EventComponent
   ],
   imports: [
     BrowserModule
