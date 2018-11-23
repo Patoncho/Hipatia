4dd1cbb466930bb99acb1ce7c1853a0a2ba6c1ab
Patoncho <42099978+Patoncho@users.noreply.github.com>
2018-11-23 13:50:02 -0300

    Carrousel

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { BodyComponent } from './body/body.component';
 import { FooterComponent } from './footer/footer.component';
 import { MapsComponent } from './maps/maps.component';
 import { EventComponent } from './event/event.component';
+import { DirectComponent } from './direct/direct.component';
 
 @NgModule({
   declarations: [
@@ -15,7 +16,8 @@ import { EventComponent } from './event/event.component';
     BodyComponent,
     FooterComponent,
     MapsComponent,
-    EventComponent
+    EventComponent,
+    DirectComponent
   ],
   imports: [
     BrowserModule
