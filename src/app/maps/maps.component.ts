13ccf259361f177a07638cfce8907abe45cc348b
franco <spagnolifranco58@gmail.com>
2018-11-28 17:27:21 -0300

    finalmente, despues de tener un taladro en el cerebro, creo que pude integrar un ejemplo de la api de google maps

--- src/app/maps/maps.component.ts
+++ src/app/maps/maps.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+declare const google: any;
 
 @Component({
   selector: 'app-maps',
@@ -10,6 +11,14 @@ export class MapsComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+        let mapProp = {
+            center: new google.maps.LatLng(51.508742, -0.120850),
+            zoom: 5,
+            mapTypeId: google.maps.MapTypeId.ROADMAP
+        };
+        let map = new google.maps.Map(document.getElementById("googleMap"), mapProp);
+    }
+}
   }
 
 }
