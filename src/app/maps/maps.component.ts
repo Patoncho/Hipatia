41562be8c71f5380ce8abd8899509d90d0950169
franco <spagnolifranco58@gmail.com>
2018-11-30 19:51:17 -0300

    maps deleted

--- src/app/maps/maps.component.ts
+++ src/app/maps/maps.component.ts
@@ -11,14 +11,6 @@ export class MapsComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
-        let mapProp = {
-            center: new google.maps.LatLng(43.5293101, -5.6773233),
-            zoom: 13,
-            mapTypeId: google.maps.MapTypeId.ROADMAP
-        };
-        let map = new google.maps.Map(document.getElementById("googleMap"), mapProp);
-    }
 }
-  }
 
 }
